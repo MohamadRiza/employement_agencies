ffbeab2d488429690b040d5056e61258dabf6f5b
MohamadRiza <rawufdeenriza@gmail.com>
2025-08-10 20:29:36 +0530

    changed contect.jsx in import.meta.env.vite api url one slash /

--- src/pages/Contect.jsx
+++ src/pages/Contect.jsx
@@ -28,7 +28,7 @@ const Contact = () => {
     setSuccess(false);
 
     try {
-      const res = await fetch(`${import.meta.env.VITE_API_URL}/api/messages`, {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}api/messages`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
